788e44e2f25466ca3c30d463cfe2d9f051ecae58
Dmitrii Rusinov <199-41@mail.ru>
2023-10-07 20:18:03 +0300

    win32 support step2

--- src/db/file.c
+++ src/db/file.c
@@ -12,19 +12,21 @@
 
 #include <sys/mman.h>
 
+int fd;
+
 int open_file(const char *filename) {
-    int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 
     if (fd == -1) {
         logger(LL_ERROR, __func__, "Could not open file.");
         return -1;
     }
 
-    return fd;
+    return 0
 }
 
-int close_file(int file_descriptor) {
-    int close_result = close(file_descriptor);
+int close_file() {
+    int close_result = close(fd);
 
     if (close_result != 0) {
         logger(LL_ERROR, __func__, "Could not close file with descriptor %d.", file_descriptor);
@@ -34,7 +36,7 @@ int close_file(int file_descriptor) {
     return 0;
 }
 
-int change_file_size(int fd, uint64_t new_size) {
+int change_file_size(uint64_t new_size) {
     int result = ftruncate(fd, (off_t) new_size);
 
     if (result != 0) {
@@ -46,7 +48,7 @@ int change_file_size(int fd, uint64_t new_size) {
     return 0;
 }
 
-int sync_file(int fd) {
+int sync_file() {
     int result = fsync(fd);
 
     if (result != 0) {
@@ -57,7 +59,7 @@ int sync_file(int fd) {
     return 0;
 }
 
-int munmap_file(void *file_data_pointer, uint64_t file_size, int fd) {
+int munmap_file(void *file_data_pointer, uint64_t file_size) {
     int result = munmap(file_data_pointer, file_size);
 
     if (result != 0) {
@@ -71,7 +73,7 @@ int munmap_file(void *file_data_pointer, uint64_t file_size, int fd) {
     return 0;
 }
 
-int mmap_file(int fd, void **file_data_pointer, uint64_t offset, uint64_t size) {
+int mmap_file(void **file_data_pointer, uint64_t offset, uint64_t size) {
     *file_data_pointer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (off_t) offset);
 
     if (*file_data_pointer == MAP_FAILED) {
@@ -94,7 +96,7 @@ int delete_file(const char *filename) {
     return 0;
 }
 
-uint64_t get_file_size(int fd) {
+uint64_t get_file_size() {
     off_t file_size = lseek(fd, 0, SEEK_END);
 
     if (file_size == -1) {
@@ -111,118 +113,6 @@ uint64_t get_file_size(int fd) {
 
 #include <windows.h>
 
-int open_file(const char *filename) {
-    HANDLE fd = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    if (fd == INVALID_HANDLE_VALUE) {
-        logger(LL_ERROR, __func__, "Could not open file.");
-        return -1;
-    }
-
-    return (int) fd;
-}
-
-int close_file(int file_descriptor) {
-    int close_result = CloseHandle((HANDLE) file_descriptor);
-
-    if (close_result == 0) {
-        logger(LL_ERROR, __func__, "Could not close file with descriptor %d.", file_descriptor);
-        return -1;
-    }
-
-    return 0;
-}
-
-int change_file_size(int fd, uint64_t new_size) {
-    LARGE_INTEGER file_size;
-    file_size.QuadPart = new_size;
-
-    int result = SetFilePointerEx((HANDLE) fd, file_size, NULL, FILE_BEGIN);
-
-    if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not change file file size with descriptor %d to %ld.", fd,
-               new_size);
-        return -1;
-    }
-
-    result = SetEndOfFile((HANDLE) fd);
-
-    if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not change file file size with descriptor %d to %ld.", fd,
-               new_size);
-        return -1;
-    }
-
-    return 0;
-}
-
-int sync_file(int fd) {
-    int result = FlushFileBuffers((HANDLE) fd);
-
-    if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not sync file with descriptor %d.", fd);
-        return -1;
-    }
-
-    return 0;
-}
-
-int munmap_file(void *file_data_pointer, uint64_t file_size, int fd) {
-    int result = UnmapViewOfFile(file_data_pointer);
-
-    if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not unmap file with pointer %p and file size %ld.",
-               file_data_pointer, file_size);
-        return -1;
-    }
-
-    sync_file(fd);
-
-    return 0;
-}
-
-int mmap_file(int fd, void **file_data_pointer, uint64_t offset, uint64_t size) {
-    HANDLE file_mapping = CreateFileMapping((HANDLE) fd, NULL, PAGE_READWRITE, 0, 0, NULL);
-
-    if (file_mapping == NULL) {
-        logger(LL_ERROR, __func__, "Could not create file mapping with descriptor %d, offset %ld and file size %ld.",
-               fd, offset, size);
-        return -1;
-    }
-
-    *file_data_pointer = MapViewOfFile(file_mapping, FILE_MAP_ALL_ACCESS, 0, 0, size);
-
-    if (*file_data_pointer == NULL) {
-        logger(LL_ERROR, __func__, "Could not map file with descriptor %d, offset %ld and file size %ld.", fd,
-               offset, size);
-        return -1;
-    }
-
-    return 0;
-}
-
-int delete_file(const char *filename) {
-    int result = DeleteFile(filename);
-
-    if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not delete file with name %s.", filename);
-        return -1;
-    }
-
-    return 0;
-}
-
-uint64_t get_file_size(int fd) {
-    LARGE_INTEGER file_size;
-
-    int result = GetFileSizeEx((HANDLE) fd, &file_size);
-
-    if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not get file size with descriptor %d.", fd);
-        return -1;
-    }
-
-    return (uint64_t) file_size.QuadPart;
-}
 
 #endif
--- src/db/file.h
+++ src/db/file.h
@@ -10,18 +10,18 @@
 
 int open_file(const char *filename);
 
-int close_file(int file_descriptor);
+int close_file();
 
-int change_file_size(int fd, uint64_t new_size);
+int change_file_size(uint64_t new_size);
 
-int sync_file(int fd);
+int sync_file();
 
-int munmap_file(void *file_data_pointer, uint64_t file_size, int fd);
+int munmap_file(void *file_data_pointer, uint64_t file_size);
 
-int mmap_file(int fd, void **file_data_pointer, uint64_t offset, uint64_t size);
+int mmap_file(void **file_data_pointer, uint64_t offset, uint64_t size);
 
 int delete_file(const char *filename);
 
-uint64_t get_file_size(int fd);
+uint64_t get_file_size();
 
 #endif //LLP_DATABASE_FILE_H
