a4ef616e686e1da3111a1ba4453d77081a30bb0f
ruskaof <336769@niuitmo.ru>
2023-10-10 08:10:57 +0300

    windows?

--- src/db/file.c
+++ src/db/file.c
@@ -143,28 +143,30 @@ int delete_file(const char *filename) {
 
 #include <windows.h>
 
-HANDLE file_handle;
+HANDLE hFile;
 
 int open_file(const char *filename) {
     logger(LL_DEBUG, __func__, "Opening file %s.", filename);
 
-    file_handle = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    hFile = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    if (file_handle == INVALID_HANDLE_VALUE) {
+    if (hFile == INVALID_HANDLE_VALUE) {
         logger(LL_ERROR, __func__, "Could not open file.");
         return -1;
     }
+
+    file_size = (uint64_t) GetFileSize(hFile, NULL);
 
     return 0;
 }
 
 int close_file() {
-    logger(LL_DEBUG, __func__, "Closing file with handle %p.", file_handle);
+    logger(LL_DEBUG, __func__, "Closing file with handle %p.", hFile);
 
-    int close_result = CloseHandle(file_handle);
+    int close_result = CloseHandle(hFile);
 
     if (close_result == 0) {
-        logger(LL_ERROR, __func__, "Could not close file with handle %p.", file_handle);
+        logger(LL_ERROR, __func__, "Could not close file with handle %p.", hFile);
         return -1;
     }
 
@@ -172,33 +174,33 @@ int close_file() {
 }
 
 int change_file_size(uint64_t new_size) {
-    logger(LL_DEBUG, __func__, "Changing file file size with handle %p to %ld.", file_handle, new_size);
+    logger(LL_DEBUG, __func__, "Changing file file size with handle %p to %ld.", hFile, new_size);
 
-    int result = SetFilePointer(file_handle, (LONG) new_size, NULL, FILE_BEGIN);
-
+    int result = SetFilePointer(hFile, (LONG) new_size, NULL, FILE_BEGIN);
     if (result == INVALID_SET_FILE_POINTER) {
-        logger(LL_ERROR, __func__, "Could not change file file size with handle %p to %ld.", file_handle,
+        logger(LL_ERROR, __func__, "Could not change file file size with handle %p to %ld.", hFile,
                new_size);
         return -1;
     }
 
-    int set_end_result = SetEndOfFile(file_handle);
-    if (set_end_result == 0) {
-        logger(LL_ERROR, __func__, "Could not change file file size with handle %p to %ld.", file_handle,
+    result = SetEndOfFile(hFile);
+    if (result == 0) {
+        logger(LL_ERROR, __func__, "Could not change file file size with handle %p to %ld.", hFile,
                new_size);
         return -1;
     }
 
+    file_size = new_size;
     return 0;
 }
 
 int sync_file() {
-    logger(LL_DEBUG, __func__, "Syncing file with handle %p.", file_handle);
+    logger(LL_DEBUG, __func__, "Syncing file with handle %p.", hFile);
 
-    int result = FlushFileBuffers(file_handle);
+    int result = FlushFileBuffers(hFile);
 
     if (result == 0) {
-        logger(LL_ERROR, __func__, "Could not sync file with handle %p.", file_handle);
+        logger(LL_ERROR, __func__, "Could not sync file with handle %p.", hFile);
         return -1;
     }
 
@@ -206,8 +208,19 @@ int sync_file() {
 }
 
 int munmap_file() {
+    if (file_size == 0) {
+        return 0;
+    }
+
     logger(LL_DEBUG, __func__, "Unmapping file with pointer %p and file size %ld.",
            file_data_pointer, file_size);
+
+    int sync_result = sync_file();
+    if (sync_result == -1) {
+        logger(LL_ERROR, __func__, "Could not sync file with pointer %p and file size %ld.",
+               file_data_pointer, file_size);
+        return -1;
+    }
 
     int result = UnmapViewOfFile(file_data_pointer);
 
@@ -217,31 +230,26 @@ int munmap_file() {
         return -1;
     }
 
-    int sync_result = sync_file();
-    if (sync_result == -1) {
-        logger(LL_ERROR, __func__, "Could not sync file with pointer %p and file size %ld.",
-               file_data_pointer, file_size);
-        return -1;
-    }
-
     return 0;
 }
 
 int mmap_file() {
-    logger(LL_DEBUG, __func__, "Mapping file with handle %p and file size %ld.", file_handle, file_size);
+    logger(LL_DEBUG, __func__, "Mapping file with handle %p and file size %ld.", hFile, file_size);
 
-    HANDLE file_mapping_handle = CreateFileMapping(file_handle, NULL, PAGE_READWRITE, 0, 0, NULL);
+    if (file_size == 0) {
+        return 0;
+    }
 
-    if (file_mapping_handle == NULL) {
-        logger(LL_ERROR, __func__, "Could not create file mapping with handle %p and file size %ld.", file_handle,
+    HANDLE hMapFile = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, 0, NULL);
+    if (hMapFile == NULL) {
+        logger(LL_ERROR, __func__, "Could not create file mapping for file with handle %p and file size %ld.", hFile,
                file_size);
         return -1;
     }
 
-    file_data_pointer = MapViewOfFile(file_mapping_handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
-
+    file_data_pointer = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
     if (file_data_pointer == NULL) {
-        logger(LL_ERROR, __func__, "Could not map file with handle %p and file size %ld.", file_handle, file_size);
+        logger(LL_ERROR, __func__, "Could not map file with handle %p and file size %ld.", hFile, file_size);
         return -1;
     }
 
